7ba1d4301d559055d4b469f7817aac300847a633
taitd-gms <94522008+taitddev@users.noreply.github.com>
2023-03-23 19:44:12 +0700

    feat: add next-auth
    -fix(prismadb.ts): change const prisma to var prisma to allow reassignment
    -feat(prismadb.ts): add support for global prisma instance and NODE_ENV check
    -feat(package.json): add dependencies for bcrypt and next-auth, add types for bcrypt
    -feat(auth): add credentials provider for email and password authentication
    -feat(auth): add authorization logic for email and password authentication
    -feat(auth): add Prisma adapter for NextAuth
    -feat(auth): add JWT secret and session strategy
    -feat(auth): add debug mode for development environment

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  const prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
-const prisma = new PrismaClient();
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
-export default prisma;
+export default client;
