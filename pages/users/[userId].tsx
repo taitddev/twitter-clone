88d777fe4fbb94cf1af904e9d3c8a796a39a802e
taitddev <94522008+taitddev@users.noreply.github.com>
2023-03-26 16:38:02 +0700

    feat: add loading spinner
    - feat(package.json): add react-spinners dependency
    - feat([userId].tsx): add loading spinner while user data is being fetched

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,16 +1,20 @@
 import Header from "@/components/Header";
 import useUser from "@/hooks/useUser";
 import { useRouter } from "next/router";
-import React from "react";
+import { ClipLoader } from "react-spinners";
 
 const Profile = () => {
   const router = useRouter();
   const { userId } = router.query;
 
   const { user, isLoading } = useUser(userId as string);
-  console.log("🚀 ~ file: [userId].tsx:11 ~ Profile ~ user:", user);
 
-  if (isLoading || !user) return null;
+  if (isLoading || !user)
+    return (
+      <div className="flex h-full items-center justify-center">
+        <ClipLoader color="lightblue" size={80} />
+      </div>
+    );
 
   return (
     <>
